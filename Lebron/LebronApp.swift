611a0ce69614431b096fb7a5dd785a10eeaca9a1
Ayo <aogunkinle@lcs.on.ca>
2024-01-23 14:13:09 -0500

    Initial Commit

---
+++ Lebron/LebronApp.swift
@@ -0,0 +1,17 @@
+//
+//  LebronApp.swift
+//  Lebron
+//
+//  Created by Ayodeji Ogunkinle on 2024-01-23.
+//
+
+import SwiftUI
+
+@main
+struct LebronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
