5ef02c9bb2c3a32d175e0a67de115ee7ccb12993
Ayo <aogunkinle@lcs.on.ca>
2024-01-23 14:41:34 -0500

    Partly done first trading card.

--- Lebron/ContentView.swift
+++ Lebron/ContentView.swift
@@ -9,12 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+           
+            
+            RoundedRectangle(cornerRadius: 25)
+                .foregroundColor(.darkPurple)
+                
+                
+            
+            RoundedRectangle(cornerRadius: 25)
+                .foregroundColor(.goldV2)
+                .padding()
+                .padding()
+        Image("lbj")
+                .resizable()
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .aspectRatio(contentMode: .fit)
+                .padding(.init(top: 50, leading: 45, bottom: 70, trailing: 45))
+            
+            
+            
         }
+        
         .padding()
     }
 }
